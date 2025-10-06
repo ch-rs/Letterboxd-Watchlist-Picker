1ef2fddb5bf32d42f59df4b14d6660a284d7c335
Chris Saggerson <hey@ch.rs>
2025-10-06 12:45:59 +0200

    Fix wrong dataset attribute for film name

--- api/index.go
+++ api/index.go
@@ -17,24 +17,24 @@ import (
 	"github.com/gocolly/colly/v2/extensions"
 )
 
-//Film struct for http response
+// Film struct for http response
 type film struct {
-	Slug         string  `json:"slug"`         //url of film
-	Image        string  `json:"image_url"`    //url of image
-	ImageData    *string `json:"image_data"`   //base64 data URL of image
-	Year         string  `json:"release_year"`
-	Name         string  `json:"film_name"`
-	Length       string  `json:"film_length"`
+	Slug          string  `json:"slug"`       //url of film
+	Image         string  `json:"image_url"`  //url of image
+	ImageData     *string `json:"image_data"` //base64 data URL of image
+	Year          string  `json:"release_year"`
+	Name          string  `json:"film_name"`
+	Length        string  `json:"film_length"`
 	OriginalIndex int     `json:"original_index"` //original position in the list before shuffling (-1 if not in top)
 }
 
 // Add a new response struct that includes URLs
 type filmResponse struct {
-	Films []film  `json:"films"`
+	Films []film   `json:"films"`
 	URLs  []string `json:"urls"`
 }
 
-//struct for channel to send film and whether is has finshed a user
+// struct for channel to send film and whether is has finshed a user
 type filmSend struct {
 	film film //film to be sent over channel
 	done bool //if user is done
@@ -42,8 +42,8 @@ type filmSend struct {
 
 type toIgnore struct {
 	unreleased bool
-	short bool
-	feature bool
+	short      bool
+	feature    bool
 }
 
 type nothingReason int
@@ -76,11 +76,10 @@ func (e *nothingError) Error() string {
 }
 
 const urlscrape = "https://letterboxd.com/ajax/poster" //first part of url for getting full info on film
-const urlEnd = "std/125x187/"            // second part of url for getting full info on film
+const urlEnd = "std/125x187/"                          // second part of url for getting full info on film
 const site = "https://letterboxd.com"
 const watchlistGridClass = ".poster-grid"
 const regularListGridClass = ".poster-list"
-
 
 // func main() {
 // 	getFilmHandler := http.HandlerFunc(Handler)
@@ -102,9 +101,7 @@ func init() {
 	year = time.Now().Year()
 }
 
-
-
-//Main handler func for request
+// Main handler func for request
 func Handler(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 	log.Println(year)
@@ -165,24 +162,23 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
-
-//main scraping function
+// main scraping function
 func scrapeMain(users []string, intersect bool, ignoreList toIgnore) ([]film, []string, error) {
 	var user int = 0          //conuter for number of users increses by one when a users page starts being scraped decreses when user has finished think kinda like a semaphore
 	var totalFilms []film     //final list to hold all film
 	ch := make(chan filmSend) //channel to send films over
-	
+
 	// Track URLs being scraped
 	var scrapedURLs []string
-	
+
 	// start go routine to scrape each user
 	for _, a := range users {
 		log.Println(a)
 		user++
 		var url string
-		
+
 		if strings.Contains(a, "/") {
-			if (strings.Contains(a,"actor/") || strings.Contains(a,"director/")) {
+			if strings.Contains(a, "actor/") || strings.Contains(a, "director/") {
 				url = site + "/" + a
 				if ignoreList.short || ignoreList.feature {
 					go scrapeActorWithLength(a, ch)
@@ -196,7 +192,7 @@ func scrapeMain(users []string, intersect bool, ignoreList toIgnore) ([]film, []
 					strslice := strings.Split(a, "/")
 					url = site + "/" + strslice[0] + "/list/" + strslice[1] + "/by/added-earliest/"
 				}
-				
+
 				if ignoreList.short || ignoreList.feature {
 					go scrapeListWithLength(a, ch)
 				} else {
@@ -211,7 +207,7 @@ func scrapeMain(users []string, intersect bool, ignoreList toIgnore) ([]film, []
 				go scrapeUser(a, ch)
 			}
 		}
-		
+
 		scrapedURLs = append(scrapedURLs, url)
 	}
 	for {
@@ -236,7 +232,7 @@ func scrapeMain(users []string, intersect bool, ignoreList toIgnore) ([]film, []
 
 	var filmList []film
 	if intersect {
-		intersectList := getintersect(totalFilms,len(users))
+		intersectList := getintersect(totalFilms, len(users))
 		length := len(intersectList)
 		if length == 0 {
 			return nil, nil, &nothingError{reason: INTERSECT}
@@ -265,7 +261,6 @@ func scrapeMain(users []string, intersect bool, ignoreList toIgnore) ([]film, []
 	// Return the URLs along with the films
 	return filmList[:numFilms], scrapedURLs, nil
 }
-
 
 func scrapeUserWithLength(userName string, ch chan filmSend) {
 	url := site + "/" + userName + "/watchlist"
@@ -303,7 +298,6 @@ func scrapeList(listNameIn string, ch chan filmSend) {
 	}
 	scrape(url, regularListGridClass, ch)
 }
-
 
 // Add a new function to fetch image as base64
 func fetchImageAsBase64(imageURL string) string {
@@ -358,7 +352,7 @@ func fetchImageAsBase64(imageURL string) string {
 
 	// Encode as base64
 	base64Data := base64.StdEncoding.EncodeToString(imageData)
-	
+
 	// Return as data URL
 	return fmt.Sprintf("data:%s;base64,%s", contentType, base64Data)
 }
@@ -366,23 +360,23 @@ func fetchImageAsBase64(imageURL string) string {
 // Update the scraping functions to include image data
 func scrape(url string, posterGridClass string, ch chan filmSend) {
 	siteToVisit := url
-	posterCount := 0  // Track the number of posters processed
+	posterCount := 0 // Track the number of posters processed
 
 	ajc := colly.NewCollector()
 	filmIndex := 0
 	ajc.OnHTML("div.film-poster", func(e *colly.HTMLElement) { //secondard cleector to get main data for film
-		name := e.Attr("data-film-name")
+		name := e.Attr("data-item-full-display-name")
 		slug := e.Attr("data-film-link")
 		img := e.ChildAttr("img", "src")
 		year := getYear(e.ChildAttr("span", "title"))
-		
+
 		// Set original index for the first 3 films, -1 for the rest
 		originalIndex := -1
 		if posterCount < 3 {
 			originalIndex = posterCount
 		}
 		posterCount++
-		
+
 		// Fetch image as base64
 		var imageData string
 		if filmIndex < 10 {
@@ -390,13 +384,13 @@ func scrape(url string, posterGridClass string, ch chan filmSend) {
 		} else {
 			imageData = ""
 		}
-		
+
 		tempfilm := film{
-			Slug:  (site + slug),
-			Image: img,
-			ImageData: &imageData,
-			Year: year,
-			Name:  name,
+			Slug:          (site + slug),
+			Image:         img,
+			ImageData:     &imageData,
+			Year:          year,
+			Name:          name,
 			OriginalIndex: originalIndex,
 		}
 		ch <- ok(tempfilm)
@@ -425,28 +419,27 @@ func scrape(url string, posterGridClass string, ch chan filmSend) {
 
 }
 
-
 func scrapeWithLength(url string, posterGridClass string, ch chan filmSend) { //is slower so is own function
 	siteToVisit := url
-	posterCount := 0  // Track the number of posters processed
-	
+	posterCount := 0 // Track the number of posters processed
+
 	ajc := colly.NewCollector()
 	extensions.RandomUserAgent(ajc)
 	filmIndex := 0
 	ajc.OnHTML("div#film-page-wrapper", func(e *colly.HTMLElement) {
-		name :=e.ChildAttr("div.react-component","data-item-name")
-		slug := e.ChildAttr("div.react-component","data-item-link")
+		name := e.ChildAttr("div.react-component", "data-item-name")
+		slug := e.ChildAttr("div.react-component", "data-item-link")
 		img := e.ChildAttr("img", "src")
-		year := getYear(e.ChildAttr("div.react-component","data-item-full-display-name"))
+		year := getYear(e.ChildAttr("div.react-component", "data-item-full-display-name"))
 		lenght := e.ChildText("p.text-footer")
-		
+
 		// Set original index for the first 3 films, -1 for the rest
 		originalIndex := -1
 		if posterCount < 3 {
 			originalIndex = posterCount
 		}
 		posterCount++
-		
+
 		// Fetch image as base64
 		var imageData string
 		if filmIndex < 10 {
@@ -454,14 +447,14 @@ func scrapeWithLength(url string, posterGridClass string, ch chan filmSend) { //
 		} else {
 			imageData = ""
 		}
-		
+
 		tempfilm := film{
-			Slug:  (site + slug),
-			Image: img,
-			ImageData: &imageData,
-			Year: year,
-			Name:  name,
-			Length: strings.TrimSpace(before(lenght,"mins")),
+			Slug:          (site + slug),
+			Image:         img,
+			ImageData:     &imageData,
+			Year:          year,
+			Name:          name,
+			Length:        strings.TrimSpace(before(lenght, "mins")),
 			OriginalIndex: originalIndex,
 		}
 		ch <- ok(tempfilm)
@@ -492,11 +485,11 @@ func scrapeWithLength(url string, posterGridClass string, ch chan filmSend) { //
 
 }
 
-//fix later
+// fix later
 func scrapeActor(actor string, ch chan filmSend) {
 	siteToVisit := site + "/" + actor
 	fmt.Println(siteToVisit)
-	posterCount := 0  // Track the number of posters processed
+	posterCount := 0 // Track the number of posters processed
 
 	c := colly.NewCollector()
 	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 100})
@@ -505,23 +498,23 @@ func scrapeActor(actor string, ch chan filmSend) {
 		slug := e.Attr("data-film-link")
 		img := e.ChildAttr("img", "src")
 		year := e.Attr("data-film-release-year")
-		
+
 		// Set original index for the first 3 films, -1 for the rest
 		originalIndex := -1
 		if posterCount < 3 {
 			originalIndex = posterCount
 		}
 		posterCount++
-		
+
 		// Fetch image as base64
 		imageData := fetchImageAsBase64(makeBiggerActor(img))
-		
+
 		tempfilm := film{
-			Slug:  (site + slug),
-			Image: makeBiggerActor(img),
-			ImageData: &imageData,
-			Year: year,
-			Name:  name,
+			Slug:          (site + slug),
+			Image:         makeBiggerActor(img),
+			ImageData:     &imageData,
+			Year:          year,
+			Name:          name,
 			OriginalIndex: originalIndex,
 		}
 		ch <- ok(tempfilm)
@@ -540,11 +533,11 @@ func scrapeActor(actor string, ch chan filmSend) {
 
 }
 
-//fix later
+// fix later
 func scrapeActorWithLength(actor string, ch chan filmSend) {
 	siteToVisit := site + "/" + actor
 	log.Println(siteToVisit)
-	posterCount := 0  // Track the number of posters processed
+	posterCount := 0 // Track the number of posters processed
 
 	c := colly.NewCollector()
 	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 100})
@@ -554,18 +547,18 @@ func scrapeActorWithLength(actor string, ch chan filmSend) {
 	filmIndex := 0
 	ajc.OnHTML("div#film-page-wrapper", func(e *colly.HTMLElement) {
 		name := e.ChildText("span.frame-title")
-		slug := e.ChildAttr("div.film-poster","data-film-link")
+		slug := e.ChildAttr("div.film-poster", "data-film-link")
 		img := e.ChildAttr("img", "src")
-		year := e.ChildAttr("div.film-poster","data-film-release-year")
+		year := e.ChildAttr("div.film-poster", "data-film-release-year")
 		lenght := e.ChildText("p.text-footer")
-		
+
 		// Set original index for the first 3 films, -1 for the rest
 		originalIndex := -1
 		if posterCount < 3 {
 			originalIndex = posterCount
 		}
 		posterCount++
-		
+
 		// Fetch image as base64
 		var imageData string
 		if filmIndex < 10 {
@@ -573,14 +566,14 @@ func scrapeActorWithLength(actor string, ch chan filmSend) {
 		} else {
 			imageData = ""
 		}
-		
+
 		tempfilm := film{
-			Slug:  (site + slug),
-			Image: img,
-			ImageData: &imageData,
-			Year: year,
-			Name:  name,
-			Length: strings.TrimSpace(before(lenght,"mins")),
+			Slug:          (site + slug),
+			Image:         img,
+			ImageData:     &imageData,
+			Year:          year,
+			Name:          name,
+			Length:        strings.TrimSpace(before(lenght, "mins")),
 			OriginalIndex: originalIndex,
 		}
 		ch <- ok(tempfilm)
@@ -629,7 +622,7 @@ func getintersect(filmSlice []film, numOfUsers int) []film {
 	for _, entry := range filmSlice {
 		i, _ := keys[entry]
 		if i < (numOfUsers - 1) {
-			keys[entry] ++
+			keys[entry]++
 		} else {
 			list = append(list, entry)
 		}
@@ -692,36 +685,36 @@ func ignoreFeature(filmSlice []film) []film {
 }
 
 func before(value string, a string) string {
-    // Get substring before a string.
-    pos := strings.Index(value, a)
-    if pos == -1 {
-        return ""
-    }
-    return value[0:pos]
+	// Get substring before a string.
+	pos := strings.Index(value, a)
+	if pos == -1 {
+		return ""
+	}
+	return value[0:pos]
 }
 
 func after(value string, a string) string {
-    // Get substring before a string.
-    pos := strings.Index(value, a)
-    if pos == -1 {
-        return ""
-    }
-    return value[pos:]
+	// Get substring before a string.
+	pos := strings.Index(value, a)
+	if pos == -1 {
+		return ""
+	}
+	return value[pos:]
 }
 
 func contains(s []string, e string) bool {
-    for _, a := range s {
-        if a == e {
-            return true
-        }
-    }
-    return false
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
 
 func getYear(name string) string {
 	year := after(name, "(")
 	if year != "" {
-		return string(year[1:len(year)-1])
+		return string(year[1 : len(year)-1])
 	} else {
 		return "0"
 	}
@@ -731,9 +724,9 @@ func getYear(name string) string {
 func whatToIgnore(ignoreString string) toIgnore {
 	ignoreList := strings.Split(ignoreString, ",")
 	return toIgnore{
-		unreleased: contains(ignoreList,"unreleased"),
-		short: contains(ignoreList, "shorts"),
-		feature: contains(ignoreList, "feature"),
+		unreleased: contains(ignoreList, "unreleased"),
+		short:      contains(ignoreList, "shorts"),
+		feature:    contains(ignoreList, "feature"),
 	}
 }
 
